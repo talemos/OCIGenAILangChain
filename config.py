cac577484ec48a445898cffb21b4ae8f5ccbc3be
talemos <talemos@me.com>
2024-05-08 16:48:10 -0300

    updates for testing

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-compartment_id = "<COMPARTMENT ID>"
-auth_profile = "<AUTH PROFILE>"
+compartment_id = "ocid1.compartment.oc1..aaaaaaaaoktk7plmojxogbad7ws6g2ideob73ja5stbizet3mvqsj62bhsxq"
+auth_profile = "GENAI"
 service_endpoint = "https://inference.generativeai.us-chicago-1.oci.oraclecloud.com"
 embeddings_model_id = "cohere.embed-english-v3.0"
 llm_model_id = "cohere.command" #meta.llama-2-70b-chat
